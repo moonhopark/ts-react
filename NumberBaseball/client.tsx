07682f868698680cdb3055702b8c8fc139d044c7
moonhopark <moonho4199@naver.com>
2022-07-02 16:36:10 +0900

    feat: 숫자야구

---
+++ NumberBaseball/client.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+import NumberBaseball from './NumberBaseball';
+
+ReactDom.render(<NumberBaseball />, document.querySelector('#root'));
